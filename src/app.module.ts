a939d65d29015f0e0040349c30b6c0bb0322986c
Nhattruong2308 <truongbvn@runsystem.net>
2025-05-30 17:55:21 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
+  imports: [
+    // MongooseModule.forRoot(
+    //   'mongodb+srv://truongbvn:Truong2308%40@cluster0.sakla1x.mongodb.net/',
+    // ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { genSaltSync, hashSync } from 'bcryptjs';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  getHashPassword = (password: string) => {
+    const salt = genSaltSync(10);
+    const hash = hashSync(password, salt);
+    return hash;
+  };
+  // async create(createUserDto: CreateUserDto) {
+  async create(email: string, password: string, name: string) {
+    const hashPassword = this.getHashPassword(password);
+    let user = await this.userModel.create({
+      email,
+      password: hashPassword,
+      name,
+    });
+    return user;
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
